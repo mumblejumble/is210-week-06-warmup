cb5fdf888740ef86ecaccf0462975420cc8a1d3e
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-03 02:22:42 +0000

    tuple and list added

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tuple and List"""
+
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
