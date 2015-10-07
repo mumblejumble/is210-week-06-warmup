bda8029aa54e63e7eaad9658d73bcfe8dd465b8b
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-10-07 02:13:30 +0000

    documentation mended

--- task_04.py
+++ task_04.py
@@ -6,7 +6,14 @@
 def process_data(data):
     """returns a tuple of the sum and the mean for a given dataset
 
-    arguments:
+    args:
+        data: could be passed by integers. 
+        
+        mysum: serves as a counter for mynum to be added on, until the end
+        of the given dataset.
+
+        mynum: refers to every number given in data.
+        
         myave: returns mean of sum of the given dataset
 
         for loop: every item (mynum) in the dataset is being added to mysum (0)
