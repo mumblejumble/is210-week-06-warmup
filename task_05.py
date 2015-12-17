fff9214b6593858a30197422f9017a5039b17ff0
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-12-17 02:26:18 +0000

    redo

--- task_05.py
+++ task_05.py
@@ -4,15 +4,15 @@
 
 
 def flip_keys(to_flip):
-    """using for loop to reverse the order of the inner sequence
+    """This function returns a list with inner list sequence flipped.
 
-    args:
+    Args:
         to_flip(list): could be passed with interger or string.
 
-    returns:
+    Returns:
         returns a list with flipped inner sequence
 
-    examples:
+    Examples:
         >>>LIST = [(1, 2, 3), 'abc']
         >>>NEW = flip_keys(LIST)
         >>>LIST is NEW
@@ -20,12 +20,9 @@ def flip_keys(to_flip):
         >>>print LIST
         [(3,2,1), 'cba']
     """
-
     counter = 0
-
     for flipvar in to_flip:
         re_myvar = (flipvar[::-1])
         to_flip[counter] = re_myvar
         counter += 1
-
     return to_flip
